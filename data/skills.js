ce990c838e81bb334cf2822aef42ad7b2d5d847d
Alexander Pensavalle <alexanderpensavalle@Alexanders-MacBook-Pro.local>
2020-08-25 21:04:14 -0400

    Dev Skills Part One Done

---
+++ data/skills.js
@@ -0,0 +1,13 @@
+const skills = [
+    {skill: 'OOP'},
+    {skill: 'JS'},
+    {skill: 'Java'},
+    {skill: 'HTML/CSS'},
+    {skill: 'Data Structures'},
+  ];
+  
+  module.exports = {
+    getAll: function() {
+      return skills;
+    }
+  };
---
+++ server.js
@@ -0,0 +1,44 @@
+// Require modules
+const express = require('express');
+const path = require('path');
+// require the skills "database"
+const skillsDB = require('./data/skills');
+ 	
+// Create the Express app
+const app = express();
+    
+// Configure the app (app.set) aka specify the ejs/express view engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));//tell Express where all of our views can be found
+    
+// Mount middleware (app.use)
+    
+    
+// Mount / Define routes
+app.get('/', function(req, res) {
+    res.redirect('/home');
+  });
+
+app.get('/home', function(req, res) {
+    //res.send('<h1>Welcome Home</h1>');
+    res.render('home');
+});
+
+app.get('/skills', function(req, res) {
+    res.render('skills/index', {
+      skills: skillsDB.getAll()
+    });
+  });
+
+
+
+
+
+
+
+
+    
+// Tell the app to listen on port 3000
+app.listen(3000, function() {
+  console.log('Listening on port 3000');
+});
